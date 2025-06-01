572ca2ae77d952483b9bcf340a9a8d6d83d76d7f
pasharudenko <pasharudenko210@gmail.com>
2025-06-02 01:11:17 +0300

    first commit

--- infrastructure/src/app.ts
+++ infrastructure/src/app.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CdkStack } from "./stacks";
+import { MessageServiceStack } from "./stacks";
 
 const app = new cdk.App();
-new CdkStack(app, "CdkStack", {
+new MessageServiceStack(app, "MessageServiceStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
